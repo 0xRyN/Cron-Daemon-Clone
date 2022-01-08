7f073d853f437b2df44f6ae0094112e22e0e4245
DanyHamimi <63753999+DanyHamimi@users.noreply.github.com>
2022-01-08 20:10:28 +0100

    Add files via upload

---
+++ src/cassini/terminate.c
@@ -0,0 +1,28 @@
+#include "terminate.h"
+int terminate(char* REQ_PIPE_PATH, char* RES_PIPE_PATH, uint16_t operation){
+    int REQ_FD = open(REQ_PIPE_PATH, O_WRONLY);
+        // Convert and write operation
+            uint16_t op = htobe16(operation);
+            if (write(REQ_FD, &op, 2) < 2) {
+                perror("REQUEST_TERMINATE : Write error...");
+                close(REQ_FD);
+                return -1;
+                return 1;
+            }
+            close(REQ_FD);
+            // Wait for the response
+            int RES_FD = open(RES_PIPE_PATH, O_RDONLY);
+            uint16_t reptype;
+            read(RES_FD, &reptype, 2);
+
+            // Checking if the daemon response is OK...
+            if (be16toh(reptype) == SERVER_REPLY_ERROR) {
+                perror(
+                    " CLIENT_REQUEST_REMOVE_TASK: : Daemon responded with an "
+                    "error code, exiting...");
+                close(RES_FD);
+                return -1;
+            }
+            return 1;
+            close(RES_FD);
+}
