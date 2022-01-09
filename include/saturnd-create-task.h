6c09999d07dcb95ac1a0f249d0216c1e5f7c5c22
hamimi <dany.hamimi@gmail.com>
2022-01-09 13:29:06 +0100

    fixed includes in saturnd-create-task.h to make make work

--- include/saturnd-create-task.h
+++ include/saturnd-create-task.h
@@ -1,23 +1,12 @@
 #ifndef SATURND_CREATE_TASK_H
 #define SATURND_CREATE_TASK_H
 
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <unistd.h>
-
-#include "client-request.h"
-#include "command.h"
-#include "cstring.h"
 #include "operations.h"
-#include "server-reply.h"
-#include "timing-text-io.h"
-#include "timing.h"
-#include "util.h"
 
 int handle_taskid(char *path);
 int handle_create_task(char *buf, int nbtasks);
+int handle_timing(char *path);
+int handle_command(char *path);
+int handle_runs(char *path);
 
 #endif
